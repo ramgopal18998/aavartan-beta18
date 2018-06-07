23c76c50535895e54c2c0bcd3a8e6dc439460016
Akash Gayakwad <akashgayakwad123@gmail.com>
2018-06-07 07:21:26 +0530

    customized

--- Base Template/js/main.js
+++ Base Template/js/main.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
 
   // fullpage customization
   $('#fullpage').fullpage({
-    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#5C832F', '#B8B89F'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
     navigation: true,
     slidesNavigation: true,
     css3: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection'],
     menu: '#menu',
 
     /*
@@ -39,7 +39,7 @@ $(document).ready(function(){
       if(index == 5) {
         $('#fp-nav').show();
       }
-    },*/ 
+    },
 //for avoiding scrolling in last section slide 2 and changing styling for slide 2
     afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
       if(anchorLink == 'fifthSection' && slideIndex == 1) {
@@ -65,6 +65,6 @@ $(document).ready(function(){
         $header_top.css('background', 'rgba(0, 47, 77, .3)');
         $nav.css('background', 'rgba(0, 47, 77, .25)');
       }
-    }
+    }*/ 
   }); 
 });
